3fe9ca06043cc7d386a3c3ff21532984247022e6
AndrewChen <chenpoanandrew@gmail.com>
2018-07-08 15:02:27 +0800

    add body-parser

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
+
+app.use(bodyParser());
+
+const defaultFakeMember = {
+  name: 'fake-member-name',
+  phone: '0930123123',
+  age: 30
+};
  
-app.get('/member/:id', function (req, res) {
+app.get('/members/:id', function (req, res) {
 
   const fakeMember = {
     id: req.params.id,
-    name: 'fake-member-name',
-    age: 30
+    ...defaultFakeMember,
+    ...req.query
   };
 
   res.send(fakeMember);
 });
+
+app.post('/members', function(req, res) {
+
+  const id = Date.now();
+
+  res.send({
+    ...defaultFakeMember,
+    ...req.body,
+    id
+  });
+});
+
  
 app.listen(3000);
